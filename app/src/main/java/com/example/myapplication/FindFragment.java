167c22ab8d50d28c7949f5351823586fa62d19d8
Daniil1380 <Tkachenko1380@yandex.ru>
2020-11-06 09:27:38 +0300

    Upload all 3 labs

--- app/src/main/java/com/example/myapplication/FindFragment.java
+++ app/src/main/java/com/example/myapplication/FindFragment.java
@@ -8,13 +8,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class ProfileFragment extends Fragment {
 
-
+public class FindFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_find, container, false);
     }
 }
--- app/src/main/java/com/example/myapplication/MenuActivity.java
+++ app/src/main/java/com/example/myapplication/MenuActivity.java
@@ -1,38 +1,43 @@
 package com.example.myapplication;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModel;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MenuActivity extends AppCompatActivity {
+    NavController navController;
+    private RecyclerView recyclerView;
+    private BibLibAdapter bibLibAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        navController = Navigation.findNavController(this, R.id.fragmentStart);
         getSupportActionBar().hide();
+
+
     }
 
     public void onFindClick(View view) {
-        Intent intent = new Intent(MenuActivity.this, FindActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-        overridePendingTransition(0,0);
-        startActivity(intent);
+        navController.navigate(R.id.findFragment);
     }
 
     public void onNewsClick(View view) {
-        Intent intent = new Intent(MenuActivity.this, NewsActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-        overridePendingTransition(0,0);
-        startActivity(intent);
+        navController.navigate(R.id.newsFragment);
     }
 
     public void onProfileClick(View view) {
-        Intent intent = new Intent(MenuActivity.this, MenuActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-        overridePendingTransition(0,0);
-        startActivity(intent);
+        navController.navigate(R.id.profileFragment);
     }
 
     @Override
